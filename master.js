6d7bcc093fda01e4aa61fbb9f2e9b09fc133c836
mostafajr10psg <parisantgerman10@gmail.com>
2025-07-20 20:02:33 +0300

    final version

--- master.js
+++ master.js
@@ -1,28 +1,12 @@
-const content = document.querySelector(".content");
-const desc = document.querySelector(".desc");
-const profileInfo = document.querySelector(".profile-info");
 const socialLinks = document.querySelector(".social-links");
 const shareIcon = document.querySelector(".share-icon");
 
-function removeProperties(ele, ...props) {
-  for (let prop of props) ele.style.removeProperty(prop);
-}
-
-function addColorsToShareIcon() {
-  shareIcon.style.cssText = `background-color: rgb(110 125 151);
-  color: var(--light-g-blue);`;
-}
-
-function reomveColorsFromShareIcon() {
-  removeProperties(shareIcon, "background-color", "color");
-}
-
 document.addEventListener("click", (e) => {
   if (e.target === shareIcon) {
-    socialLinks.style.cssText = `bottom: 115px; opacity: 1; z-index: 10; transform: translateY(0)`;
-    shareIcon.classList.add("white");
+    shareIcon.classList.add("active");
+    socialLinks.classList.add("show-social-links");
   } else if (!e.target.closest(".social-links")) {
-    removeProperties(socialLinks, "bottom", "opacity", "z-index", "transform");
-    shareIcon.classList.remove("white");
+    socialLinks.classList.remove("show-social-links");
+    shareIcon.classList.remove("active");
   }
 });
